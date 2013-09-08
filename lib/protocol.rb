4a1c5fe8b77d24fa66d6c0e5c4964b68c0791180
Shane Hanna <shane.hanna@gmail.com>
2013-09-09 03:16:35 +1000

    DSL cleanup WIP.

--- lib/protocol.rb
+++ lib/protocol.rb
@@ -1,20 +1,57 @@
-require 'protocol/package'
-require 'protocol/template'
+require 'stringio'
 
-require 'protocol/language'
-require 'protocol/language/c99'
-#require 'protocol/language/c99-portable'
-require 'protocol/language/ruby'
-#require 'protocol/language/ruby-portable'
-#require 'protocol/language/objc'
-#require 'protocol/language/objc-portable'
+module Protocol
+  module Serializer; end
 
+  Field = ::Struct.new(:name, :type, :pack, :size) do
+    include Serializer
 
-# Protocol
-#
-# Protocol generates little-endian C compartible structs suitable as a simple,
-# fast wire protocol.
-#
-# Why little-endian? Because x86, x86-64 and bi-endian ARM M3 etc. can all read
-# these structures into memory with minimal effort.
-module Protocol; end
+    def protocol_load io
+      type.kind_of?(Serializer) ? type.protocol_load(io) : io.read(size).to_s.unpack(pack).first
+    end
+
+    def protocol_dump io, value
+      type.kind_of?(Serializer) ? type.protocol_dump(io) : io.write([value].pack(pack))
+    end
+  end
+
+  class Struct
+    include Serializer
+
+    def initialize options = {}
+      options.each{|k, v| public_send(:"#{k}=", v)}
+    end
+
+    def protocol_dump io
+      type.protocol_dump io, self
+    end
+
+    class << self
+      attr_accessor :fields
+
+      def inherited klass
+        super
+        klass.fields = []
+      end
+
+      def field name, type, pack, size
+        fields.push field = Field.new(name, type, pack, size)
+        define_singleton_method(name, lambda{ field })
+        attr_accessor name
+      end
+
+      def protocol_dump io, value
+        fields.inject(0) do |bytes, field|
+          bytes + field.dump(io, value.public_send(field.name))
+        end
+      end
+
+      def protocol_load io
+        fields.inject(new) do |struct, field|
+          struct.public_send("#{field.name}=", field.load(io))
+          struct
+        end
+      end
+    end
+  end
+end
